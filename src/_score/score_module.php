19a8915eddae1eaf0b727e40b083b7613a506748
Corey Peterson <corey.peterson@ucf.edu>
2019-01-22 16:02:42 -0500

    Removed unnecessary comments from score module

--- src/_score/score_module.php
+++ src/_score/score_module.php
@@ -1,26 +1,5 @@
 <?php
-/**
- * Materia
- * It's a thing
- *
- * @package	    Materia
- * @version    1.0
- * @author     UCF New Media
- * @copyright  2011 New Media
- * @link       http://kogneato.com
- */
 
-
-/**
- * NEEDS DOCUMENTATION
- *
- * The widget managers for the Materia package.
- *
- * @package	    Main
- * @subpackage  scoring
- * @category    Modules
-  * @author      ADD NAME HERE
- */
 namespace Materia;
 
 class Score_Modules_SurveyWidget extends Score_Module
@@ -33,19 +12,6 @@ class Score_Modules_SurveyWidget extends Score_Module
 		{
 			return 100;
 		}
-		// $answers = $this->questions[$log->item_id]->answers;
-		// foreach($answers as $answer)
-		// {
-		// 	if ($log->text != null && $log->text != ''
-		// 	{
-		// 		return 100;
-		// 	}
-		// 	// if ($log->text == $answer['text'])
-		// 	// {
-		// 	// 	return $answer['value'];
-		// 	// }
-		// }
-		// return 0;
 	}
 
 }
